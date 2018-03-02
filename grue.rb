ca295e91aa440931cf468ba3a31b9a269dcc794d
Levi Bard <levi@unity3d.com>
2018-03-02 09:31:57 +0100

    Extract uri mangling to separate library

--- grue.rb
+++ grue.rb
@@ -10,11 +10,11 @@
 # General Public License for more details.
 
 require_relative 'fixedqueue'
+require_relative 'urika'
 
 require 'uri'
 require 'psych'
 
-URIRE = /https?:\/\/[^\s]+/
 URLS_PER_CHANNEL = 100000 # ?
 CACHE = "#{ENV['HOME']}/.grue.yaml".freeze
 
@@ -31,7 +31,7 @@ class Grue
   # param text The text of the statement
   # returns The entries with a matching URL (including this statement), or nil
   def process_statement(channel, nick, text)
-    return nil unless (url = Grue.get_first_url(text))
+    return nil unless (url = Urika.get_first_url(text))
 
     # TODO: Better/canonical timestamp?
     record_url(channel, nick, url, Time.now)
@@ -75,27 +75,6 @@ class Grue
       puts "Error dumping urls: #{error.to_s}"
     end
   end
-
-  # Gets the first http[s] url from a string
-  # param message The string to be searched
-  # returns The first url (sanitized), or nil
-  def self.get_first_url(message)
-    return nil unless (match = URIRE.match(message))
-
-    uri = URI.parse(match.to_s())
-    return nil unless (uri && (uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)))
-
-    # Probably suboptimal performance
-    sanitized = "#{uri.host}#{uri.path}"
-    sanitized.gsub!(/\/amp(\/)?/, '\1') # fuck amp
-    sanitized.gsub!(/\/$/, '') # strip trailing slash for consistency between posters
-    return sanitized unless uri.query
-
-    query = uri.query
-    query.gsub!(/&?utm_[\w]+=[^&]*/, '') # utm_whatever is irrelevant
-    query.gsub!(/&?(aqs|sourceid|ie|redirect_source)=[^&]*/, '') # don't care about these either
-    return uri.query.empty? ? sanitized : "#{sanitized}?#{query}"
-  end # get_first_url
 end # Grue
 
 if (__FILE__ == $0)
@@ -105,27 +84,6 @@ if (__FILE__ == $0)
     def setup()
       @grue = Grue.new()
     end # setup
-
-    def test_uri_validation
-      uris = [
-        [ 'http://google.com', 'google.com' ],
-        [ 'https://google.com/', 'google.com' ],
-        [ 'htp://google.com', nil ],
-        [ 'ftp://google.com', nil ],
-        [ 'I would like you to see http://google.com', 'google.com' ],
-        [ 'I would like you to see https://google.com', 'google.com' ],
-        [ 'I would like you to see htp://google.com', nil ],
-        [ 'I would like you to see ftp://google.com', nil ],
-        [ 'htp://google.com https://google.com ttp://google.com', 'google.com' ],
-        [ 'htp://google.com http://google.com/amp?redirect_source=google.com', 'google.com' ],
-        [ 'https://www.google.dk/amp/search/amp?q=foo+bar+baz&oq=foo+bar+baz&aqs=chrome..69i57j0l5.1283j0j7&sourceid=chrome&ie=UTF-8&utm_source=meh', 'www.google.dk/search?q=foo+bar+baz&oq=foo+bar+baz' ],
-      ]
-
-      uris.each { |pair|
-        url = Grue.get_first_url(pair[0])
-        assert_equal(pair[1], url, "Unexpected url #{url} for input #{pair[0]}")
-      }
-    end # test_uri_validation
 
     def test_detect_gruing
       statements = [
